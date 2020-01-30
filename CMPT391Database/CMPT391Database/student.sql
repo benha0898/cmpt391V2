2457427ba1c37465553d6f4e4275816685dfb2a3
Ben Ha <benha0898@gmail.com>
2020-01-30 15:11:28 -0700

    Initial commit

---
+++ CMPT391Database/CMPT391Database/student.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[student]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [first_name] NCHAR(10) NULL, 
+    [last_name] NCHAR(10) NULL, 
+    [dob] DATE NULL, 
+    [street] NCHAR(20) NULL,
+    [city] NCHAR(20) NULL,
+    [province] NCHAR(20) NULL,
+    [country] NCHAR(20) NULL,
+    [postal] NCHAR(10) NULL,
+    [total_credits] INT NULL
+)
