26e6a35017a6c740ed9fd399452429d61336475e
Ben Ha <benha0898@gmail.com>
2020-02-06 15:27:18 -0700

    Add Populate script

---
+++ CMPT391Database/CMPT391Database/dbo/Scripts/Populate.sql
@@ -0,0 +1,26 @@
+﻿DELETE from dbo.student;
+DBCC CHECKIDENT('dbo.student', RESEED, 0);
+
+Declare @Id int
+Declare @FromDate date = '1980-01-01'
+Declare @EndDate date = '2002-12-31'
+
+Set @Id = 1
+
+While @Id <= 15000
+Begin 
+   Insert Into dbo.student (first_name, last_name, dob, street, city, province, country, postal, total_credits)
+   values (
+        'First ' + CAST(@Id as nchar(20)),
+        'Last ' + CAST(@Id as nchar(20)),
+        dateadd(day, rand(checksum(NEWID()))*(1+datediff(day, @FromDate, @EndDate)), @FromDate),
+        'Street ' + CAST(@Id as nchar(20)),
+        'City ' + CAST(FLOOR(rand()*(6-1)+1) as nchar(20)),
+        'Province ' + CAST(FLOOR(rand()*(4-1)+1) as nchar(20)),
+        'Country ' + CAST(FLOOR(rand()*(3-1)+1) as nchar(20)),
+        'Postal ' + CAST(@Id as nchar(20)),
+        CAST(FLOOR(rand()*(120-0+1)+0) as int));
+
+   Print @Id
+   Set @Id = @Id + 1
+End
--- CMPT391Database/CMPT391Database/dbo/Tables/student.sql
+++ CMPT391Database/CMPT391Database/dbo/Tables/student.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[student]
 (
-	[id] INT NOT NULL PRIMARY KEY, 
-    [first_name] NCHAR(10) NULL, 
-    [last_name] NCHAR(10) NULL, 
+	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [first_name] NCHAR(20) NULL, 
+    [last_name] NCHAR(20) NULL, 
     [dob] DATE NULL, 
     [street] NCHAR(20) NULL,
     [city] NCHAR(20) NULL,
     [province] NCHAR(20) NULL,
     [country] NCHAR(20) NULL,
-    [postal] NCHAR(10) NULL,
+    [postal] NCHAR(20) NULL,
     [total_credits] INT NULL
 )
