f6975b3ef0057a0fee55183f46d2945e7ff455e0
rehmana66 <rehmana6@mymacewan.ca>
2020-03-21 14:16:30 -0600

    basic view for datawarehouse

---
+++ CMPT391Database/DataWarehouse/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataWarehouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            instructor_dropdown.Items.Add("Department");
+            semester_dropdown.Items.Add("Term");
+            semester_dropdown.Items.Add("Year");
+            student_dropdown.Items.Add("Major");
+            student_dropdown.Items.Add("Status");
+            course_dropdown.Items.Add("Subject");
+            course_dropdown.Items.Add("Department");
+            course_dropdown.Items.Add("Credit");
+        }
+    }
+}
